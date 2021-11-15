66cac77d9ed41802f4fbbb94d00cb95c06666854
원두영 <sofcom@naver.com>
2021-11-15 20:32:52 +0900

    Intent 초기 UI 작업

--- app/src/main/java/com/nasplus/intentexample01/MainActivity.kt
+++ app/src/main/java/com/nasplus/intentexample01/MainActivity.kt
@@ -2,10 +2,16 @@ package com.nasplus.intentexample01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOtherApp.setOnClickListener {
+            Toast.makeText(this, "다른 페이지로 이동", Toast.LENGTH_SHORT).show()
+        }
     }
 }
